971d18260850ed074888f6c13d6b38d403759488
Heitor Chang <heitorchang@gmail.com>
2020-11-06 18:03:27 -0300

    autocomplete

--- insert_add.php
+++ insert_add.php
@@ -10,6 +10,7 @@ $stmt->execute([":na" => $_POST['name'],
                 ":ev" => $_POST['event_date'],
                 ":qt" => $_POST['qty'],
                 ":un" => $_POST['unit'],
-                ":pr" => $_POST['price']]);
+                ":pr" => str_replace(",", ".", $_POST['price'])
+]);
 
 header('Location: insert.php');
