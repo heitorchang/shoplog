c2c803260ef9eb7893a23dcd0e7f8b17b7c8579f
Heitor Chang <heitorchang@gmail.com>
2020-11-15 13:41:40 -0300

    prevent empty submit

--- insert_add.php
+++ insert_add.php
@@ -7,14 +7,19 @@ function stripAccents($str) {
     return strtr(utf8_decode($str), utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'), 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
 }
 
-$stmt = $dbh->prepare("insert into shoplog (name, notes, store, event_date, qty, unit, price) values (:na, :no, :st, :ev, :qt, :un, :pr)");
-$stmt->execute([":na" => stripAccents($_POST['name']),
-                ":no" => stripAccents($_POST['notes']),
-                ":st" => stripAccents($_POST['store']),
-                ":ev" => $_POST['event_date'],
-                ":qt" => str_replace(",", ".", $_POST['qty']),
-                ":un" => $_POST['unit'],
-                ":pr" => str_replace(",", ".", $_POST['price'])
-]);
+if ($_POST['name'] == '' || $_POST['price'] == '') {
+  $redirect_page = "insert_error.php";
+} else {
+  $stmt = $dbh->prepare("insert into shoplog (name, notes, store, event_date, qty, unit, price) values (:na, :no, :st, :ev, :qt, :un, :pr)");
+  $stmt->execute([":na" => stripAccents($_POST['name']),
+                  ":no" => stripAccents($_POST['notes']),
+                  ":st" => stripAccents($_POST['store']),
+                  ":ev" => $_POST['event_date'],
+                  ":qt" => str_replace(",", ".", $_POST['qty']),
+                  ":un" => $_POST['unit'],
+                  ":pr" => str_replace(",", ".", $_POST['price'])
+  ]);
+  $redirect_page = "index.php";
+}
 
-header('Location: index.php');
+header('Location: ' . $redirect_page);
---
+++ insert_error.php
@@ -0,0 +1,13 @@
+<?php
+
+require("db.php");
+include("header.php");
+
+session_start();
+?>
+
+<p>
+Could not insert data (missing name or price).
+
+<a href="index.php">Try again</a>
+</p>
